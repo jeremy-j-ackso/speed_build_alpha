02bccf8064464eee799a78a1d613f6ca849a22f9
Jeremy Jackson <jeremy_j@ackso.net>
2018-02-03 16:09:34 -0700

    Starting to build the application entry point.

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const nano = require('nano')('http://node_user:thisisreallysecure@localhost:5984');
+
+const app = express();
+app.get;
+
+app.post;
+
+app.listen('3000');
